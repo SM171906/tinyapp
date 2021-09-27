88a33622b31decba431003b0dafb0d50b039f7a8
Smiti <mishra.smiti06@gmail.com>
2021-09-27 14:06:15 -0400

    installed ejs & set it on express file

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express ();
 const PORT = 8080;
-
+app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -21,3 +21,4 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
